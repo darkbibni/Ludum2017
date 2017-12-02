35ecd712fb8d3f72a461cde54ae8a38e94ac5452
BERTAUX Loris <l.bertaux@rubika-edu.com>
2017-12-02 13:56:30 +0100

    UI and room switch

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+
+	public Camera cam;
+	public Room[] rooms;
+
+	public void MoveToRoom(int roomIndex) {
+		Vector3 newCamPos = rooms [roomIndex].GetRoomPosition ();
+		newCamPos.z = cam.transform.position.z;
+
+		cam.transform.position = newCamPos;
+	}
+}
