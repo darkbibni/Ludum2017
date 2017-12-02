ea1109a7c3c7c2f3b781ab8bba32ceae2ee5aa9a
BERTAUX Loris <l.bertaux@rubika-edu.com>
2017-12-02 14:38:20 +0100

    GameManager and SplashScreen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager singleton;
+
+	public GameObject blackScreen;
+	public GameObject gameOverPanel;
+
+	public RoomManager roomMgr;
+
+	void Awake() {
+		SingletonThis();
+	}
+
+	private void Restart() {
+		// Fade out.
+		blackScreen.SetActive (false);
+		gameOverPanel.SetActive (false);
+
+		roomMgr.MoveToRoom (-1);
+	}
+
+	private void GameOver() {
+		// Fade In
+		blackScreen.SetActive (true);
+		gameOverPanel.SetActive (true);
+	}
+
+	private void SingletonThis() {
+		if (singleton == null) {
+			singleton = this;
+		} else {
+			Destroy (gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	[ContextMenu("GameOver")]
+	public void SimulateGO() {
+		GameOver ();
+	}
+
+	[ContextMenu("Restart")]
+	public void SimulateRestart() {
+		Restart ();
+	}
+}
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -6,9 +6,20 @@ public class RoomManager : MonoBehaviour {
 
 	public Camera cam;
 	public Room[] rooms;
+	public GameObject splashScreen;
 
 	public void MoveToRoom(int roomIndex) {
-		Vector3 newCamPos = rooms [roomIndex].GetRoomPosition ();
+
+		Vector3 newCamPos;
+
+		if (roomIndex < 0 || roomIndex > rooms.Length) {
+			newCamPos = splashScreen.transform.position;
+		}
+
+		else {
+			newCamPos = rooms [roomIndex].GetRoomPosition ();
+		}
+				
 		newCamPos.z = cam.transform.position.z;
 
 		cam.transform.position = newCamPos;
